a980fc39365c2afe7ee012f98d5323e7d757d05e
acer <fsaidov167@gmail.com>
2022-08-17 19:19:09 +0500

    pdpui3

--- lib/introPage.dart
+++ lib/introPage.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
-
+import 'package:intropage/homepage.dart';
 import 'Strings.dart';
-import 'homepage.dart';
-
-
 class IntroPage extends StatefulWidget {
-  static final String id = "intro_page";
+  const IntroPage({Key? key}) : super(key: key);
+  static const String id = 'IntroPage';
 
   @override
-  _IntroPageState createState() => _IntroPageState();
+  State<IntroPage> createState() => _IntroPageState();
 }
 
 class _IntroPageState extends State<IntroPage> {
-
   late PageController _pageController;
   int currentIndex = 0;
 
   @override
   void initState() {
-    _pageController = PageController(
-        initialPage: 0
-    );
+    // TODO: implement initState
     super.initState();
+    _pageController = PageController(
+      initialPage: 0
+    );
   }
-
   @override
   void dispose() {
-    _pageController.dispose();
+    // TODO: implement dispose
     super.dispose();
+    _pageController.dispose();
   }
 
   @override
@@ -40,43 +38,42 @@ class _IntroPageState extends State<IntroPage> {
         actions: [
           GestureDetector(
             onTap: (){
-              Navigator.pushReplacementNamed(context, HomePage.id);
+              Navigator.pushReplacementNamed(context,HomePage.id );
             },
             child: Padding(
               padding: EdgeInsets.only(right: 20,left: 20),
-              child: Text('Skip',style: TextStyle(color: Colors.green,fontSize: 18,fontWeight: FontWeight.w400),),
+              child: Text('Skip',style: TextStyle(color: Colors.green,fontWeight: FontWeight.w400,fontSize: 18),),
             ),
-          ),
-
+          )
         ],
       ),
       body: Stack(
         alignment: Alignment.bottomCenter,
         children: [
           PageView(
-            onPageChanged: (int page) {
-              setState(() {
+            onPageChanged: (int page){
+              setState((){
                 currentIndex = page;
               });
             },
             controller: _pageController,
             children: [
               makePage(
-                  image: 'assets/images/img.png',
-                  title: Strings.stepOneTitle,
-                  content: Strings.stepOneContent
+                image: 'assets/images/img.png',
+                title: Strings.stepOneTitle,
+                content: Strings.stepOneContent,
               ),
               makePage(
-                  reverse: true,
-                  image: 'assets/images/img_1.png',
-                  title: Strings.stepTwoTitle,
-                  content: Strings.stepTwoContent
+                image: 'assets/images/img_1.png',
+                title: Strings.stepTwoTitle,
+                content: Strings.stepTwoContent
               ),
               makePage(
-                  image: 'assets/images/img_2.png',
-                  title: Strings.stepThreeTitle,
-                  content: Strings.stepThreeContent
-              ),
+                image: 'assets/images/img_2.png',
+                title: Strings.stepTwoTitle,
+                content: Strings.stepThreeContent
+              )
+
             ],
           ),
           Container(
@@ -85,67 +82,52 @@ class _IntroPageState extends State<IntroPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: _buildIndicator(),
             ),
-          ),
+          )
         ],
       ),
     );
   }
-
-  Widget makePage({image, title, content, reverse = false}) {
+  Widget makePage ({image,title,content,reverse = false}){
     return Container(
-      padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
+      padding: EdgeInsets.only(left: 50,right: 50,bottom: 60),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-
           Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+            children: [
+              Padding(padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Image.asset(image),
               ),
               SizedBox(height: 10,),
             ],
           ),
-          Text(title, style: TextStyle(
-              color: Colors.green,
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-          ),),
-          SizedBox(height: 30,),
-          Text(content, textAlign: TextAlign.center, style: TextStyle(
-              color: Colors.grey,
-              fontSize: 20,
-              fontWeight: FontWeight.w400
-          ),),
+          Text(content,textAlign: TextAlign.center,style: TextStyle(color: Colors.grey,fontSize: 20,fontWeight: FontWeight.w400),)
         ],
       ),
+
     );
   }
-
-  Widget _indicator(bool isActive) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
-      height: 6,
-      width: isActive ? 30 : 6,
+  Widget _indicator(bool isActive){
+    return AnimatedContainer(duration: Duration(milliseconds: 300),
+    height: 6,
+      width: isActive ? 30 :6,
       margin: EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(5)
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(5)
       ),
     );
   }
-
-  List<Widget> _buildIndicator() {
+  List <Widget> _buildIndicator(){
     List<Widget> indicators = [];
-    for (int i = 0; i<3; i++) {
-      if (currentIndex == i) {
+    for(int i=0;i<3;i++){
+      if(currentIndex ==i){
         indicators.add(_indicator(true));
-      } else {
+      }
+      else{
         indicators.add(_indicator(false));
       }
     }
     return indicators;
   }
-
 }
